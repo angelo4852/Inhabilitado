3ba85ff901bcbd25492e6be6e1c0ed3360f7c4e9
Gilberto Peralta Leyva <gilberto.peralta@puebla.gob.mx>
2024-07-02 13:01:05 -0600

    Cambios GPL 02-07-2024

--- ConstanciaNoInhabilitado/Client/obj/Debug/net7.0/ConstanciaNoInhabilitado.Client.AssemblyInfo.cs
+++ ConstanciaNoInhabilitado/Client/obj/Debug/net7.0/ConstanciaNoInhabilitado.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConstanciaNoInhabilitado.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9994711d3480a6fe1dbee733451890a3e79071")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConstanciaNoInhabilitado.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConstanciaNoInhabilitado.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
